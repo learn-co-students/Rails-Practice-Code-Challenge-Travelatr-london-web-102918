32b3c997994742ff7cfea83cfa055bc6c126b9b3
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-29 17:16:07 +0000

    About 1h30

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,21 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :age, numericality: { only_integer: true }
+  validates :age, numericality: { greater_than: 0 }
+  validates :bio, presence: true
+  validates :bio, length: { minimum: 30 }
+
+  def featured_post
+    "This is my featured post"
+  end
+
+  def most_written_about
+    destinations = self.destinations.map {|d| d.name}
+    # destinations.max_by {|d| d.count}
+  end
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,17 @@
 class Destination < ApplicationRecord
+  has_many :posts
+  has_many :bloggers, through: :posts
+
+  def most_recent
+    self.posts.last(5)
+  end
+
+  def featured_post
+    "This is my featured post"
+  end
+
+  def average_blogger_age
+    15
+  end
 
 end
